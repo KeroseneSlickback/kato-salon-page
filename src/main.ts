2877cae6b3a6d7afdb3cea54a52b8ed68eb7a3b1
Will <mitchellspaur@gmail.com>
2022-08-16 19:57:58 +0900

    finalized nav mobile button

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 const primaryNav = document.querySelector(".primary-navigation");
 const navToggle = document.querySelector(".mobile-nav-toggle");
+const navSpanLines = document.querySelectorAll(".nav-span");
 
 navToggle?.addEventListener("click", () => {
   const visibility = primaryNav?.getAttribute("data-visible");
   if (visibility === "false") {
     primaryNav?.setAttribute("data-visible", "true");
+    navSpanLines.forEach((span) => {
+      span.setAttribute("data-visible", "true");
+    });
   } else {
     primaryNav?.setAttribute("data-visible", "false");
+    navSpanLines.forEach((span) => {
+      span.setAttribute("data-visible", "false");
+    });
   }
 });
 
