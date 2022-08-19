7fd0266cb69acea4e7f4372d15419aad67327f3f
Will <mitchellspaur@gmail.com>
2022-08-19 14:36:21 +0900

    mobile nav menu and icon styling finisheD

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,37 @@
 const primaryNav = document.querySelector(".primary-navigation");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const navSpanLines = document.querySelectorAll(".nav-span");
+const navBookButtonContainer = document.querySelector(
+  ".nav-book-button-container"
+);
+const navBookButton = document.querySelector(".nav-book-button");
+const mobileBackdrop = document.querySelector(".mobile-backdrop");
 
-navToggle?.addEventListener("click", () => {
+const mobileNavSlide = () => {
   const visibility = primaryNav?.getAttribute("data-visible");
   if (visibility === "false") {
     primaryNav?.setAttribute("data-visible", "true");
     navSpanLines.forEach((span) => {
       span.setAttribute("data-visible", "true");
     });
+    navBookButtonContainer?.setAttribute("data-visible", "true");
+    navBookButton?.setAttribute("data-visible", "true");
+    mobileBackdrop?.setAttribute("data-visible", "true");
   } else {
     primaryNav?.setAttribute("data-visible", "false");
     navSpanLines.forEach((span) => {
       span.setAttribute("data-visible", "false");
     });
+    mobileBackdrop?.setAttribute("data-visible", "false");
+    const colorChangeTimeout = setTimeout(() => {
+      navBookButtonContainer?.setAttribute("data-visible", "false");
+      navBookButton?.setAttribute("data-visible", "false");
+    }, 200);
+    clearTimeout(colorChangeTimeout);
   }
-});
+};
+
+navToggle?.addEventListener("click", mobileNavSlide);
+mobileBackdrop?.addEventListener("click", mobileNavSlide);
 
 export {};
